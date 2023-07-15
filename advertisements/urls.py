095cb6dc8d14c6f88d2e6c766bfd6b99d1175549
Nellenan <nikitasergeev06310@gmail.com>
2023-07-15 13:29:36 +0300

    Занятие №5. Код с урока.

--- advertisements/urls.py
+++ advertisements/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, top_sellers
 
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('top-sellers/', top_sellers),
 ]
 
