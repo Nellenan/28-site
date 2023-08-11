f8b41bdb0fe5dc4f33462c60d6343d489cb5d3c4
Nellenan <nikitasergeev06310@gmail.com>
2023-08-11 09:15:17 +0300

    Занятие №7. Задание №4

--- advertisements/models.py
+++ advertisements/models.py
@@ -1,16 +1,28 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
 
 
 class Advertisement(models.Model):
-    title = models.CharField(max_length=80)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=12, decimal_places=2)
-    auction = models.BooleanField(help_text='Отметьте, если торг уместен.obj = AD')
-    created_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=80, verbose_name='заголовок')
+    description = models.TextField("описание")
+    price = models.DecimalField("цена", max_digits=12, decimal_places=2)
+    auction = models.BooleanField('возможность торга', help_text='Отметьте, если торг уместен.obj = AD')
+    created_at = models.DateTimeField('дата создания', auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+
+    @admin.display(description="Дата создания")
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M')
+            return format_html('<span style="color: green"> Сегодня в {}', created_time)
+        return self.created_at
 
     class Meta:
         db_table = 'advertisement'
+        verbose_name = 'объявление'
+        verbose_name_plural = 'объявления'
 
     def __str__(self):
         return f'<Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})'
